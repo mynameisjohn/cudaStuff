4496b1bff04e6f339bdddbb3a1a3d21d6b2ac875
mynameisjohn <mynameisjohnj@gmail.com>
2015-04-26 08:35:59 -0400

    Kernels in main

--- main.cpp
+++ main.cpp
@@ -1,8 +1,19 @@
 #include "misc.h"
 #include "gsRelax.h"
 
+//Fix block size for N <= 1024
+
 int main(){
-	relax(1024, 1, 100, 0.1f);
+	const uint32_t N(2048), nIt(1000);
+	const float minRes(0.1f), thresh = -1.f;
+	int dim = 2;
+
+	AllGPUAllCpu(N, nIt);
+	SubGpuAllCpu(N, nIt);
+	SubGpuAllCpu_R(N, nIt,thresh);
+	AllGpuSubCpu(N, nIt);
+
+	relax(N, dim, nIt, minRes);
 	
 	return 0;
 }
